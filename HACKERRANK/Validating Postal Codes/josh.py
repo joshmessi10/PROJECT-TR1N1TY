8f5f242be8713d7918b43010e0fd9028fadc189f
Josh Sebastián López Murcia <65347220+joshmessi10@users.noreply.github.com>
2025-05-09 11:05:46 -0500

    Create josh.py

---
+++ HACKERRANK/Validating Postal Codes/josh.py
@@ -0,0 +1,12 @@
+regex_integer_in_range = r"^[1-9][0-9]{5}$"	# Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(?=([0-9]).{1}\1)"	# Do not delete 'r'.
+# (?=...) -> Lookahead: check ahead without moving
+# ([0-9]) / (\d) -> Capture a digit
+#.{n} -> Skip n digits
+#\1 Match the same digit as captured
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
