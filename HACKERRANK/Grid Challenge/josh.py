36e4d15703c29c6e8fee68c94e2a1ffe80a7c9c1
Josh Sebastián López Murcia <65347220+joshmessi10@users.noreply.github.com>
2025-05-29 16:59:48 -0500

    Create josh.py

---
+++ HACKERRANK/Grid Challenge/josh.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    arr = []
+    for i in grid:
+        arr.append(sorted(list(i)))
+    print(arr)
+    for i in range(len(arr)-1):
+        for j in range(len(arr[i])):
+            if arr[i][j] > arr[i+1][j]:
+                return "NO"
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
