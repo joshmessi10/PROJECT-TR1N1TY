4b8baa54b54b68d537d79ef921c3813e25f940f1
Josh Sebastián López Murcia <65347220+joshmessi10@users.noreply.github.com>
2025-05-29 00:02:06 -0500

    Update josh-extra.py

--- LEETCODE/Two Sum II/josh-extra.py
+++ LEETCODE/Two Sum II/josh-extra.py
@@ -1,3 +1,15 @@
+#Normal Iteration
+"""
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i+1, j+1]
+                if numbers[i] > target and numbers[j] > target:
+                    break
+"""
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         result = []
